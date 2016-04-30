4782a942dbfa6646117a6a468647560c0cc6a22a
jrice189 <jrice189@gmail.com>
2016-04-30 17:10:59 +0000

    Fixes to the date validation functions

--- server.js
+++ server.js
@@ -28,11 +28,11 @@ function isNaturalDate(str) {
     var arr=str.split(" ");
     if (arr.length!==3) return false;
     if (arr[0].indexOf(/\d/)!==-1) return false;
-    if (arr[1].splice(arr[1].length-1,1).indexOf(/\D/)!==-1) return false;
+    if (arr[1].indexOf(/\D/)!==-1) return false;
     if (arr[2].indexOf(/\D/)!==-1) return false;
     if (arr[1].charAt(arr[1].length-1)!==",") return false
     if (arr[0].toLowerCase()!=="january" &&
-        arr[0].toLowerCase()!=="febuary" &&
+        arr[0].toLowerCase()!=="february" &&
         arr[0].toLowerCase()!=="march" &&
         arr[0].toLowerCase()!=="april" &&
         arr[0].toLowerCase()!=="may" &&
@@ -43,10 +43,45 @@ function isNaturalDate(str) {
         arr[0].toLowerCase()!=="october" &&
         arr[0].toLowerCase()!=="november" &&
         arr[0].toLowerCase()!=="december") return false;
-    /*switch (arr[0].toLowerCase()) {
+    switch (arr[0].toLowerCase()) {
         case "january":
-            if arr[1].splice(arr[1].length-1,1)
-    }*/
+            if (Number(arr[1].slice(0,arr[1].length-1,1))>31) return false;
+        		break;
+        case "february":
+            if (Number(arr[1].slice(0,arr[1].length-1,1))>29) return false;
+            if (Number(arr[1].slice(0,arr[1].length-1,1))==29 && (Number(arr[2])%4!==0 || (Number(arr[2])%4===0) && Number(arr[2])%100===0 && Number(arr[2])%400!==0)) return false;
+        		break;
+        case "march":
+            if (Number(arr[1].slice(0,arr[1].length-1,1))>31) return false;
+        		break;
+        case "april":
+            if (Number(arr[1].slice(0,arr[1].length-1,1))>30) return false;
+        		break;
+        case "may":
+            if (Number(arr[1].slice(0,arr[1].length-1,1))>31) return false;
+        		break;
+        case "june":
+            if (Number(arr[1].slice(0,arr[1].length-1,1))>30) return false;
+        		break;
+        case "july":
+            if (Number(arr[1].slice(0,arr[1].length-1,1))>31) return false;
+        		break;
+        case "august":
+            if (Number(arr[1].slice(0,arr[1].length-1,1))>31) return false;
+        		break;
+        case "september":
+            if (Number(arr[1].slice(0,arr[1].length-1,1))>30) return false;
+        		break;
+        case "october":
+            if (Number(arr[1].slice(0,arr[1].length-1,1))>31) return false;
+        		break;
+        case "november":
+            if (Number(arr[1].slice(0,arr[1].length-1,1))>30) return false;
+        		break;
+        case "december":
+            if (Number(arr[1].slice(0,arr[1].length-1,1))>31) return false;
+        		break;
+    }
     return true;
 }
 
