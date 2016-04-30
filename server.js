8e20b77e03d9a1c0b6a4a55ff864bcd9fbb7d305
jrice189 <jrice189@gmail.com>
2016-04-30 17:43:58 +0000

    Fixed date response format

--- server.js
+++ server.js
@@ -9,12 +9,12 @@ app.get('/', function(req, res) {
 
 app.get ('/:str', function(req, res) {
     var str=req.params.str;
-    var d=moment(str, ["X","MMM MMMM-D DD-YYYY"]);
+    var d=moment(str, ["X","MMMM D, YYYY"]);
     var unix=null;
     var nat=null;
     if (d.isValid()) {
         unix=d.format("X");
-        nat=d.format("MMM MMMM-D DD-YYYY");
+        nat=d.format("MMMM D, YYYY");
     }
     res.type('application/json')
     res.send({"unix":unix, "natural":nat});
